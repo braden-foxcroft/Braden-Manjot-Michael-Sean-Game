52e4c3755209dca008ea8115ff10038e36613119
Sean.mccreary <sean.p.mccreary@gmail.com>
2020-02-12 17:06:55 -0700

    ACTUAL commit!

---
+++ Let's Build a Game/src/com/tutorial/main/KeyInput.java
@@ -0,0 +1,16 @@
+package com.tutorial.main;
+
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
+public class KeyInput extends KeyAdapter {
+	
+	public void keyPressed(KeyEvent e) {
+		int key = e.getKeyCode();
+	}
+	
+	public void keyRealeased(KeyEvent e) {
+		
+	}
+
+}
