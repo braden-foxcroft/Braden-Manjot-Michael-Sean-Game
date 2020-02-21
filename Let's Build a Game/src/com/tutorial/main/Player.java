3fbcf49cebeddcd99b03c835a903bdc4a665b98c
Braden <bradenfoxcroft@gmail.com>
2020-02-20 17:57:17 -0700

    A stable red box with basic behavior

--- Let's Build a Game/src/com/tutorial/main/Player.java
+++ Let's Build a Game/src/com/tutorial/main/Player.java
@@ -5,12 +5,43 @@ import java.awt.Graphics;
 
 public class Player extends GameObject{
 
+	private boolean goRight = true;
+	private boolean goLeft = false;
+	private int downH = 0;
+	
 	public Player(int x, int y, ID id) {
 		super(x, y, id);
 	}
 
 	public void tick() {
-		
+		if (this.downH <= this.y && !this.goLeft && !this.goRight) {
+			if (this.x <= 0) {
+				this.goRight = true;
+			}
+			else
+			{
+				this.goLeft = true;
+			}
+		}
+		else if (this.x >= 635-50 && this.goRight) {
+			this.goRight = false;
+			this.downH += 50;
+		}
+		else if (this.x < 0 && this.goLeft) {
+			this.goLeft = false;
+			this.downH += 50;
+		}
+		if (goRight) {
+			this.x++;
+		}
+		else if(goLeft)
+		{
+			this.x--;
+		}
+		else
+		{
+			this.y++;
+		}
 	}
 
 	public void render(Graphics g) {
